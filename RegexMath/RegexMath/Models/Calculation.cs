46f3065040547d1337c60d4cc14538fd76a08427
loliprotector <kaworinakano@gmail.com>
2019-06-09 07:19:41 +0800

    Finish first iteration of math engine

---
+++ RegexMath/RegexMath/Models/Calculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegexMath.Models
+{
+    public class Calculation
+    {
+        public string Regex { get; }
+        public Calculate Operation { get;  }
+        public delegate string Calculate(string input);
+
+        public Calculation(string regex, Calculate operation)
+        {
+            Regex = regex;
+            Operation = operation;
+        }
+    }
+}
